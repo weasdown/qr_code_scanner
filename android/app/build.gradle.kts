8093987d56dcab13eca4327c9d9bfd7173ec5438
weasdown <34220924+weasdown@users.noreply.github.com>
2025-06-09 11:24:06 +0100

    fix: fix ndk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.weasdown.qr_code_scanner"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973" // originally: flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
