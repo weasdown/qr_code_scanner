38f5d88303990283831762144bdd27ee67598a87
weasdown <34220924+weasdown@users.noreply.github.com>
2025-06-09 11:25:59 +0100

    feat: add placeholder camera_preview.dart

---
+++ lib/camera_preview.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+/// A widget to display a preview of what the camera is pointing at.
+class CameraPreview extends StatelessWidget {
+  const CameraPreview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
