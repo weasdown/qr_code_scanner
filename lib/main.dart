fed9c505a0008a018e29c0631633051a099de378
weasdown <34220924+weasdown@users.noreply.github.com>
2025-06-09 11:58:31 +0100

    feat: replace camera_preview.dart with working CameraPreview implementation from camera package

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-import 'camera_preview.dart';
 import 'gallery_button.dart';
 
-void main() {
+late List<CameraDescription> _cameras;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  _cameras = await availableCameras();
+
   runApp(const MyApp());
 }
 
@@ -64,11 +70,54 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class QRScannerHomeScreen extends StatelessWidget {
+class QRScannerHomeScreen extends StatefulWidget {
   const QRScannerHomeScreen({super.key});
 
   @override
+  State<QRScannerHomeScreen> createState() => _QRScannerHomeScreenState();
+}
+
+class _QRScannerHomeScreenState extends State<QRScannerHomeScreen> {
+  late CameraController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(_cameras[0], ResolutionPreset.max);
+    controller
+        .initialize()
+        .then((_) {
+          if (!mounted) {
+            return;
+          }
+          setState(() {});
+        })
+        .catchError((Object e) {
+          if (e is CameraException) {
+            switch (e.code) {
+              case 'CameraAccessDenied':
+                // TODO: Handle access errors here.
+                break;
+              default:
+                // TODO: Handle other errors here.
+                break;
+            }
+          }
+        });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Align(
@@ -81,7 +130,13 @@ class QRScannerHomeScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Expanded(child: const CameraPreview()),
+            Expanded(
+              child: DecoratedBox(
+                decoration: BoxDecoration(border: Border.all()),
+                child: CameraPreview(controller),
+              ),
+            ),
+
             Padding(
               padding: EdgeInsets.symmetric(vertical: 20.0),
               child: const SizedBox(height: 50, child: GalleryButton()),
