c497703cfe369f052e770280e6056d1011d80a55
weasdown <34220924+weasdown@users.noreply.github.com>
2025-06-09 09:56:52 +0100

    feat: specify camera dependency and configuration requirements

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.weasdown.qr_code_scanner"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 21 // originally: flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
